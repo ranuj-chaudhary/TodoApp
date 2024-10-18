57e0bbb4fc6c271bf17e2354d1b4240a61d4ab68
Ranuj Chaudhary <ranujchoudhary@gmail.com>
2024-10-18 16:06:48 +0530

    custom list with feature

--- src/pages/CustomList.js
+++ src/pages/CustomList.js
@@ -1,8 +1,17 @@
+import Tasks from '../components/Tasks/Tasks';
+import {useCustomList} from '../hooks/useCustomList';
 
 export default function CustomList() {
+  const {
+    handleCompleteTask,
+    handleIncompleteTask,
+  } = useCustomList()
+  
+
   return (
     <div className="custom-list">
-     
+      <Tasks toggleName="Incomplete" onFilterTask={handleIncompleteTask} />
+      <Tasks toggleName="Completed" onFilterTask={handleCompleteTask} />
     </div>
   );
 }
